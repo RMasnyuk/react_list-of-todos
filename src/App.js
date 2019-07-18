310516df71871ae69c42f7ac19a7481389879f29
Rostislav Masniuk <rostislav.masnyuk.98@gmail.com>
2019-07-18 13:46:53 +0300

    list-of-todos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import TodoList from "./components/TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todoList: [],
+      users: [],
+      loaded: false,
+      isLoading: false,
+      disabled: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.sortTable = this.sortTable.bind(this);
+    this.loadData = this.loadData.bind(this);
+  }
+
+  loadData(url) {
+    return fetch(url)
+      .then(res => res.json())
+  }
+
+  handleClick() {
+    this.setState({
+      isLoading: true,
+      disabled: true
+    });
+
+    Promise.all([
+      this.loadData('https://jsonplaceholder.typicode.com/todos'),
+      this.loadData('https://jsonplaceholder.typicode.com/users'),
+    ])
+      .then(([todos, users]) => this.setState({
+          loaded: true,
+          todoList: todos,
+          users: users
+        })
+      )
+  }
+
+  sortTable() {
+    this.setState({
+      todoList: this.state.todoList.sort((a, b) => a.title.localeCompare(b.title))
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        {this.state.loaded
+          ? <TodoList list={this.state.todoList} users={this.state.users} sort={this.sortTable}/>
+          : (
+            <div className="container">
+              <button onClick={this.handleClick} disabled={this.state.disabled}>Load</button>
+              {this.state.isLoading ? <div id='load'>Loading...</div> : null}
+            </div>
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
